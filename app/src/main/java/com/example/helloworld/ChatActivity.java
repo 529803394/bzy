36994541391d184b103f25627fb28e8cb3e61a38
bzy <bzy@local.dev>
2026-06-19 11:11:12 +0000

    v2.5.0 APK: https://fars.ee/tD1k.apk

--- app/src/main/java/com/example/helloworld/ChatActivity.java
+++ app/src/main/java/com/example/helloworld/ChatActivity.java
@@ -20,6 +20,7 @@ import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
